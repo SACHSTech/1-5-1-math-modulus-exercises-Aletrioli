3db6fe2249fe76e96f39206ed0a09b8a3d73dbc4
Pasta <Tmartins2008@icloud.com>
2024-02-14 13:57:49 -0500

    Circumfrence

--- Circumference.java
+++ Circumference.java
@@ -1,13 +1,26 @@
 class Circumference extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that computes the circumfrence of a circle given a radius from the user
+  * @author: Thomas Martins
   */
   
   public void run() {
     
-    // start coding here
+    // declare variables
+    double dblRadius;
+    double dblCircumfrence;
+
+    // get radius
+    System.out.println("");
+    dblRadius = readDouble("Input the radius of a circle in centimetres: ");
+
+    // calculate circumfrence
+    dblCircumfrence = dblRadius * 2 * Math.PI;
+
+    // output results
+    System.out.println("");
+    System.out.println("The circumfrence of a circle with a radius of " + dblRadius + " is " + dblCircumfrence + " centimetres.");
     
   }
 }
